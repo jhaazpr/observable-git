8afbf928c739c3a399da7776b1145689e0b6e249
Jasper O'Leary <jhaazpr@gmail.com>
2022-07-29 16:36:11 -0700

    fetches correctly

--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,16 @@ import Control.Monad.IO.Class
 import Data.Aeson
 import Data.Maybe (fromJust)
 import Data.Monoid ((<>))
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import GHC.Generics
 import Network.HTTP.Req
 import qualified Data.ByteString.Char8 as B
 import qualified Text.URI as URI
 
+import Secret (url)
+
 main :: IO ()
 main = runReq defaultHttpConfig $ do
-  let n :: Int
-      n = 5
-  bs <- req GET (https "httpbin.org" /: "bytes" /~ n) NoReqBody bsResponse mempty
+  bs <- req GET url NoReqBody bsResponse mempty
   liftIO $ B.putStrLn (responseBody bs)
 
