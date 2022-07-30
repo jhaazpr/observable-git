f1bd948d9bd9ed131f6d2ccd4820ef8bccd549d4
Jasper O'Leary <jhaazpr@gmail.com>
2022-07-30 09:22:21 -0700

    typechecks okay with liftIO

--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,23 @@ import Control.Monad.IO.Class
 import Data.Aeson
 import Data.Maybe (fromJust)
 import Data.Monoid ((<>))
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, unpack)
 import GHC.Generics
 import Network.HTTP.Req
 import qualified Data.ByteString.Char8 as B
 import qualified Text.URI as URI
 
-import Secret (url)
+import Secret (url, notebookName)
+
+directory :: String
+directory = "notebooks/"
+
+filepath :: String
+filepath = directory ++ unpack notebookName
 
 main :: IO ()
 main = runReq defaultHttpConfig $ do
   bs <- req GET url NoReqBody bsResponse mempty
-  liftIO $ B.putStrLn (responseBody bs)
+  -- liftIO $ B.putStrLn (responseBody bs)
+  liftIO $ B.writeFile filepath (responseBody bs)
 
